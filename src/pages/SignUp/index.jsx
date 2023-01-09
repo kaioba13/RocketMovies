9b807e801a78c21a4b5d0ba0c685fc540e3f1b09
Kaio Fagundes <kaiofagundess15@gmail.com>
2023-01-09 15:53:11 -0300

    função cadastro de usuario

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -4,8 +4,34 @@ import { Link } from "react-router-dom";
 import { Button } from "../../components/Button";
 import { ButtonText } from "../../components/ButtonBack";
 import { AiOutlineArrowLeft } from "react-icons/ai";
+import { useState } from "react";
+
+import { api } from "../../services/api";
 
 export function SignUp() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function HandleSingUp() {
+    if (!name || !email || !password) {
+      return alert("Preencha todos os campos");
+    }
+
+    api
+      .post("/users", { name, email, password })
+      .then(() => {
+        alert("Usuário cadastrado com sucesso");
+      })
+      .catch((error) => {
+        if (error.response) {
+          alert(error.response.data.message);
+        } else {
+          alert("Não foi possível cadastrar");
+        }
+      });
+  }
+
   return (
     <Container>
       <Form>
@@ -14,11 +40,23 @@ export function SignUp() {
 
         <h2>Crie sua conta</h2>
 
-        <Input placeholder="Nome" type="text" />
-        <Input placeholder="E-mail" type="text" />
-        <Input placeholder="password" type="password" />
+        <Input
+          placeholder="Nome"
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <Input
+          placeholder="E-mail"
+          type="text"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <Button title="Cadastrar" />
+        <Button title="Cadastrar" onClick={HandleSingUp} />
         <Link to="/">
           <AiOutlineArrowLeft />
           Voltar para o login
